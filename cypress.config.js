82dd371fb89424e2030bc8fe8aafc540cfc61aea
Wendel Cutrim <wendelcutrim@hotmail.com>
2024-09-21 16:41:34 -0300

    Instalado e configurado o mochawesome para gerar reports e adicionado a configuração para gravar videos no cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        video: true,
+        screenshotsFolder: 'cypress/screenshots',
+        screenshotOnRunFailure: true,
+        reporter: 'mochawesome',
+        reporterOptions: {
+            reportDir: 'cypress/results',
+            overwrite: false,
+            html: true,
+            json: false,
+            timestamp: 'mmddyyyy_HHMMss',
+        },
     },
-  },
 });
